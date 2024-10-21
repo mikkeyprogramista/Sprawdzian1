b08b8ad7b090d9fecc36f4fec9d9be6133d89659
mikkeyprogramista <mikkeyprogramista@gmail.com>
2024-10-21 11:12:05 +0200

    message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //System.out.println("Hello world!");
+
+        //String imie = "Mikolaj";
+        //if (imie == "Mikolaj") {
+        //    System.out.println("Udane");
+        //}
     }
 }
